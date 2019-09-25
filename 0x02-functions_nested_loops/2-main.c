e67c9c8129423346cd3c4e79206a6a45f96049c2
germaya <german.maya.trujillo@gmail.com>
2019-09-25 03:14:57 +0000

    #CISFUN

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet_x10 - Prints 10 times the alphabet, 
+ *in lowercase, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void);
+
+int main(void)
+{
+	print_alphabet_x10 ();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet_x10 - alphabet 10 times
+ *Return: Always .
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar (c);
+	}
+		_putchar ('\n');
+		i++;
+	}
+}
