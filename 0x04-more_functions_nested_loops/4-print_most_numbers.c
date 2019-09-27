571368cc8c960eff1d4559bc4ed8af0f74ad2afa
germaya <german.maya.trujillo@gmail.com>
2019-09-27 00:54:17 +0000

    #CISFUN

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
