3bb6c2e2f334f11eb8fd4c773a0e8f134286922b
germaya <german.maya.trujillo@gmail.com>
2019-09-29 04:09:26 +0000

    #CISFUN

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * print_to_98 - print all numbers from n o 98.
+ * @n: Integers to add
+ * Return: integers
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	} else if (n >= 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	}
+}
