76ee3d9c4423f1128b4f14a2d9ebee9d184132f3
germaya <german.maya.trujillo@gmail.com>
2019-10-01 20:50:22 +0000

    #CISFUN

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - dereferencing.
+ *
+ * @*n: is integer param
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,7 @@
+####0x05. C - Pointers, arrays and strings
+
+* C - Arrays
+* C - Pointers
+* C - Strings
+* pointers and arrays
+* data structures
