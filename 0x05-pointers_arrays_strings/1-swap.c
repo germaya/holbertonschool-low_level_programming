a46eb43ee9618b0be894e932c8d99e3330c16891
germaya <german.maya.trujillo@gmail.com>
2019-10-01 21:20:42 +0000

    #CISFUN

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * swap_int - function that swaps the values of two integers.
+ *
+ * @a: is a integer param
+ * @b: is a integer param
+ *
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+
+{
+	*a = 42;
+	*b = 98;
+}
