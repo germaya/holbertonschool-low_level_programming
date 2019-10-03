6b03d9a70ae622451c2cd30dd8da83100bbbc9be
germaya <german.maya.trujillo@gmail.com>
2019-10-03 20:23:05 +0000

    #CISFUN

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strcat - function that concatenates two strings.
+ *
+ * @dest: destination.
+ * @src: source.
+ *
+ * Return:  a string.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+	dest[i] = dest[i];
+	i++;
+	}
+	dest[i] = ' ';
+	i++;
+	while (src[j] != '\0')
+	{
+	dest[i + j] = src[j];
+	j++;
+	}
+	dest[i + j] = '\0';
+	
+	return(0);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+####0x06. C - More pointers, arrays and strings
+* Low-level programming & Algorithm  Hatching out
+####0x06. C - More pointers, arrays and strings
+* Low-level programming & Algorithm  Hatching out
+
