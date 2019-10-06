b2b57a89327e94a92bdc32a4cc658ebf1af6126f
germaya <german.maya.trujillo@gmail.com>
2019-10-06 17:29:17 +0000

    #CISFUN

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,13 +14,15 @@ int main(void)
 	double f;
 	int i;
 
-	for (i = 0; i < 50 ; i++)
+	for (i = 0; i <= 50 ; i++)
 	{
 		f = a + b;
-		printf("%.f, ", f);
+		if (i != 50)
+			printf("%.f, ", f);
+		else
+			printf("%.f,\n", f);
 		a = b;
 		b = f;
 	}
-	printf("\n");
 	return (0);
 }
