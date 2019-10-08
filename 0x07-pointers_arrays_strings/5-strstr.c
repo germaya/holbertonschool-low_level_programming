c326463b38379dcd238a6869e5d0453d0b1453a3
germaya <german.maya.trujillo@gmail.com>
2019-10-08 03:05:27 +0000

    #CISFUN

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+* _strstr - function that locates a substring.
+* @haystack: char variable
+* @needle: char variable
+* Return: 0
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *result = haystack, *fneedle = needle;
+
+	while (*haystack)
+	{
+		while (*needle)
+		{
+			if (*haystack++ != *needle++)
+			{
+				break;
+			}
+		}
+		if (!*needle)
+		{
+			return (result);
+		}
+		needle = fneedle;
+		result++;
+		haystack = result;
+	}
+	return (0);
+}
